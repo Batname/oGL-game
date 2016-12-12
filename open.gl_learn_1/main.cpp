468c42b6745fe475d00920ef35dc7ba87d8fe0e1
DenisDubinin <dadubinin@gmail.com>
2016-12-12 08:28:29 +0100

    Initial Commit

---
+++ open.gl_learn_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  open.gl_learn_1
+//
+//  Created by apple on 2016-12-12.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
