f5902fd7eefca9e19c3e4e3c8b051b056b361273
DenisDubinin <dadubinin@gmail.com>
2016-12-13 20:22:45 +0100

    glew inint

--- open.gl_learn_1/main.cpp
+++ open.gl_learn_1/main.cpp
@@ -1,22 +1,44 @@
-
-#include <GLFW/glfw3.h>
-
 #include <iostream>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define WINDOW_TITLE "OPEN GL"
 
 int main(int argc, const char * argv[]) {
+    
+    // Init glfw3 for manage window and input
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    
     glfwWindowHint(GLFW_RELEASE, GL_FALSE);
     
     GLFWwindow * window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+    
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        
+        return EXIT_FAILURE;
+    }
+    
+    // init glew for support openGL prototype of functions
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    // new we ready to call openJS here
     
     while (!glfwWindowShouldClose(window)) {
         glfwSwapBuffers(window);
