588a8321686405d83beeb1d8fdc7637edc77ec29
DenisDubinin <dadubinin@gmail.com>
2016-12-13 21:52:13 +0100

    add libs

---
+++ open.gl_learn_1/Triangle.cpp
@@ -0,0 +1,9 @@
+//
+//  Triangle.cpp
+//  open.gl_learn_1
+//
+//  Created by apple on 2016-12-13.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#include "Triangle.hpp"
---
+++ open.gl_learn_1/Triangle.hpp
@@ -0,0 +1,14 @@
+//
+//  Triangle.hpp
+//  open.gl_learn_1
+//
+//  Created by apple on 2016-12-13.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#ifndef Triangle_hpp
+#define Triangle_hpp
+
+#include <stdio.h>
+
+#endif /* Triangle_hpp */
