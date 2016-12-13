e59d300dba78812438b87cb35068082ab2029c5a
DenisDubinin <dadubinin@gmail.com>
2016-12-13 16:11:04 +0100

    glfw create window

--- open.gl_learn_1/main.cpp
+++ open.gl_learn_1/main.cpp
@@ -1,15 +1,29 @@
-//
-//  main.cpp
-//  open.gl_learn_1
-//
-//  Created by apple on 2016-12-12.
-//  Copyright © 2016 apple. All rights reserved.
-//
+
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_TITLE "OPEN GL"
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    glfwWindowHint(GLFW_RELEASE, GL_FALSE);
+    
+    GLFWwindow * window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+
     return 0;
 }
