6166dca1b89667b3a3d2c079c17b84ad7a9f740d
DenisDubinin <dadubinin@gmail.com>
2016-12-14 01:01:01 +0100

    draw triangle

--- open.gl_learn_1/Triangle.cpp
+++ open.gl_learn_1/Triangle.cpp
@@ -1,26 +1,26 @@
 #include "Triangle.hpp"
 
-
-Triangle::Triangle(ShaderLoader * shader, float * vertices) :
-    _vertices(vertices),
+Triangle::Triangle(ShaderLoader * shader) :
     _shader(shader)
 {
+    float vertices[] = {
+        0.0f,  0.5f,
+        0.5f, -0.5f,
+        -0.5f, -0.5f,
+    };
+    
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
     
     glBindVertexArray(vao);
     
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(_vertices), _vertices, GL_STATIC_COPY);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     GLint posAttrib = glGetAttribLocation(shader->getProgram(), "position");
     glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(posAttrib);
-}
-
-float * Triangle::getVertices()
-{
-    return _vertices;
 }
 
 void Triangle::render()
--- open.gl_learn_1/Triangle.hpp
+++ open.gl_learn_1/Triangle.hpp
@@ -2,31 +2,22 @@
 #ifndef Triangle_hpp
 #define Triangle_hpp
 
-#include <stdio.h>
+#include <iostream>
 #include <GL/glew.h>
 
 #include "ShaderLoader.hpp"
 
-static float ver[] = {
-    0.0f,  0.5f,
-    0.5f, -0.5f,
-    -0.5f, -0.5f,
-};
-
-
 class Triangle
 {
 public:
-    Triangle(ShaderLoader *_shader, float * vertices = ver);
+    Triangle(ShaderLoader *_shader);
     
     // getters
-    float * getVertices();
     
     // public member
     void render();
 private:
     ShaderLoader *_shader;
-    float * _vertices;
     GLuint vbo, vao;
 };
 
